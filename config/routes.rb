de5cbc90eaa9dfe80962d50ed190a092ee947d6a
Becky <github@bex.me.uk>
2016-11-17 17:26:42 +0000

    made basic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'films#top_films'   # should make this a welcome page
+  root 'welcome#index'   # should make this a welcome page
 
   get '/find_film/:imdb_id', to: 'films#find_film'
   get '/find_film', to: 'films#find_film'
