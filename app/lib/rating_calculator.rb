fb3f3f3acf515d4b18e9f6d5029bd5b4bd83e265
Becky <github@bex.me.uk>
2016-11-21 00:16:24 +0000

    doubled flix rating

--- app/lib/rating_calculator.rb
+++ app/lib/rating_calculator.rb
@@ -7,15 +7,20 @@ class RatingCalculator
       score += vote.rating_value
       count += 1
     end
-    return (score.to_f / count.to_f).round(2)
+    flix_rating = (score.to_f / count.to_f).round(2)
+    return relative_rating(flix_rating)
+  end
+
+  def relative_rating(flix_rating)
+    flix_rating * 2
   end
 
   def combined_rating(film)
     if film.flix_rating == nil
       return nil
     else
-      weighted_imdb_rating = film.imdb_rating / 4
-      weighted_tomatoes_rating = film.tomatoes_rating / 4
+      weighted_imdb_rating = film.imdb_rating / 2
+      weighted_tomatoes_rating = film.tomatoes_rating / 2
       return ((weighted_tomatoes_rating + weighted_imdb_rating + film.flix_rating) / 3).round(2)
     end
   end
